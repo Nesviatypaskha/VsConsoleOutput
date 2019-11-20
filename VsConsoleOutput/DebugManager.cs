2d336b448a718c444431ca932e3d0d67e5f040e7
Nesviatypaskha <alex.nesv@gmail.com>
2019-11-21 01:22:58 +0200

    implemented first version of connection mechanism

--- VsConsoleOutput/DebugManager.cs
+++ VsConsoleOutput/DebugManager.cs
@@ -99,14 +99,45 @@ namespace VsConsoleOutput
             return VSConstants.S_OK;
         }
 
+        private void logbreakpoints()
+        {
+            foreach (Breakpoint2 breakpoint2 in _dte.Debugger.Breakpoints)
+            {
+                Output.Log("breakpoint2.FunctionName {0}", breakpoint2.FunctionName);
+                Output.Log("breakpoint2.FileLine {0}", breakpoint2.FileLine);
+                Output.Log("breakpoint2.FunctionLineOffset {0}", breakpoint2.FunctionLineOffset);
+                //tracepoint.Macro = "MACRO";
+                //breakpoint2.Message = "Connected";
+                //breakpoint2.BreakWhenHit = false;
+            }
+        }
+
         private void addMainBreakpoint()
         {
             if (_dte != null)
             {
-                string installationPath = GetAssemblyLocalPathFrom(typeof(VsConsoleOutputPackage));
-                Output.Console("installationPath = {0}", installationPath);
                 _dte.Debugger.Breakpoints.Add("Main");
+                foreach (Breakpoint2 breakpoint2 in _dte.Debugger.Breakpoints)
+                {
+                    if(breakpoint2.FunctionName.Contains("Main(" ))
+                    {
+                        breakpoint2.Message = "Connected";
+                        breakpoint2.BreakWhenHit = false;
+                    }
+                }
             }
+        }
+        private void SetBreakWhenHit(Breakpoint2 breakpoint, bool value)
+        {
+            var messageStubbed = false;
+            if (value && string.IsNullOrEmpty(breakpoint.Message))
+            {
+                breakpoint.Message = "stub";
+                messageStubbed = true;
+            }
+            breakpoint.BreakWhenHit = value;
+            if (messageStubbed)
+                breakpoint.Message = "";
         }
 
         static string GetAssemblyLocalPathFrom(Type type)
@@ -180,6 +211,14 @@ namespace VsConsoleOutput
                         var myInfo = new DEBUG_PROPERTY_INFO[1];
                         dp2.GetPropertyInfo(enum_DEBUGPROP_INFO_FLAGS.DEBUGPROP_INFO_ALL, 0, 5000, null, 0, myInfo);
                         var outputTextWriter = myInfo[0].bstrValue;
+
+                        foreach (Breakpoint2 breakpoint2 in _dte.Debugger.Breakpoints)
+                        {
+                            if (breakpoint2.FunctionName.Contains("Main("))
+                            {
+                                breakpoint2.Delete();
+                            }
+                        }
                     }
                 }
             }
@@ -194,58 +233,80 @@ namespace VsConsoleOutput
             {
                 RedirectStdStreams(thread);
             }
-            if ((debugEvent is IDebugSessionCreateEvent2) || (riidEvent.ToString("B") == "2c2b15b7-fc6d-45b3-9622-29665d964a76"))
+            if ((debugEvent is IDebugSessionCreateEvent2) || (riidEvent.ToString("D") == "2c2b15b7-fc6d-45b3-9622-29665d964a76"))
                 Output.Log("debugEvent is IDebugSessionCreateEvent2.{0}", attributes); //"IDebugSessionCreateEvent2","2c2b15b7-fc6d-45b3-9622-29665d964a76"
-            else if ((debugEvent is IDebugProcessCreateEvent2) || (riidEvent.ToString("B") == "bac3780f-04da-4726-901c-ba6a4633e1ca"))
+            else if ((debugEvent is IDebugProcessCreateEvent2) || (riidEvent.ToString("D") == "bac3780f-04da-4726-901c-ba6a4633e1ca"))
                 Output.Log("debugEvent is IDebugProcessCreateEvent2.{0}", attributes); //"IDebugProcessCreateEvent2","bac3780f-04da-4726-901c-ba6a4633e1ca"
-            else if ((debugEvent is IDebugCustomEvent110) || (riidEvent.ToString("B") == "2615d9bc-1948-4d21-81ee-7a963f20cf59"))
+            else if ((debugEvent is IDebugCustomEvent110) || (riidEvent.ToString("D") == "2615d9bc-1948-4d21-81ee-7a963f20cf59"))
                 Output.Log("debugEvent is IDebugCustomEvent110 "); //"IDebugProcessCreateEvent2","2615d9bc-1948-4d21-81ee-7a963f20cf59"
-            else if ((debugEvent is IDebugBreakpointErrorEvent2) || (riidEvent.ToString("B") == "abb0ca42-f82b-4622-84e4-6903ae90f210"))
+            else if ((debugEvent is IDebugBreakpointErrorEvent2) || (riidEvent.ToString("D") == "abb0ca42-f82b-4622-84e4-6903ae90f210"))
                 Output.Log("debugEvent is IDebugBreakpointErrorEvent2 "); //"IDebugProcessCreateEvent2","abb0ca42-f82b-4622-84e4-6903ae90f210"
-            else if ((debugEvent is IDebugProgramCreateEvent2) || (riidEvent.ToString("B") == "96cd11ee-ecd4-4e89-957e-b5d496fc4139"))
+            else if ((debugEvent is IDebugProgramCreateEvent2) || (riidEvent.ToString("D") == "96cd11ee-ecd4-4e89-957e-b5d496fc4139"))
                 Output.Log("debugEvent is IDebugProgramCreateEvent2.{0}", attributes); //"IDebugProgramCreateEvent2","96cd11ee-ecd4-4e89-957e-b5d496fc4139"
-            else if ((debugEvent is IDebugModuleLoadEvent2) || (riidEvent.ToString("B") == "989db083-0d7c-40d1-a9d9-921bf611a4b2"))
+            else if ((debugEvent is IDebugModuleLoadEvent2) || (riidEvent.ToString("D") == "989db083-0d7c-40d1-a9d9-921bf611a4b2"))
                 Output.Log("debugEvent is IDebugModuleLoadEvent2.{0}", attributes); //"IDebugModuleLoadEvent2","989db083-0d7c-40d1-a9d9-921bf611a4b2"
-            else if ((debugEvent is IDebugThreadCreateEvent2) || (riidEvent.ToString("B") == "2090ccfc-70c5-491d-a5e8-bad2dd9ee3ea"))
+            else if ((debugEvent is IDebugThreadCreateEvent2) || (riidEvent.ToString("D") == "2090ccfc-70c5-491d-a5e8-bad2dd9ee3ea"))
             {
                 //  This interface is sent by the debug engine (DE) to the session debug manager (SDM) when a thread is created in a program being debugged.
                 Output.Log("debugEvent is IDebugThreadCreateEvent2.{0}", attributes); //"IDebugThreadCreateEvent2","2090ccfc-70c5-491d-a5e8-bad2dd9ee3ea"
             }
-            else if ((debugEvent is IDebugTelemetryDetailsEvent150) || (riidEvent.ToString("B") == "8f2652b2-cd3c-4aed-a946-a3db6f379412"))
+            else if ((debugEvent is IDebugTelemetryDetailsEvent150) || (riidEvent.ToString("D") == "8f2652b2-cd3c-4aed-a946-a3db6f379412"))
                 Output.Log("debugEvent is IDebugTelemetryDetailsEvent150.{0}", attributes); //"IDebugTelemetryDetailsEvent150","8f2652b2-cd3c-4aed-a946-a3db6f379412"
-            else if ((debugEvent is IDebugLoadCompleteEvent2) || (riidEvent.ToString("B") == "b1844850-1349-45d4-9f12-495212f5eb0b"))
+            else if ((debugEvent is IDebugLoadCompleteEvent2) || (riidEvent.ToString("D") == "b1844850-1349-45d4-9f12-495212f5eb0b"))
                 Output.Log("debugEvent is IDebugLoadCompleteEvent2.{0}", attributes); //"IDebugLoadCompleteEvent2","b1844850-1349-45d4-9f12-495212f5eb0b"
-            else if ((debugEvent is IDebugEntryPointEvent2) || (riidEvent.ToString("B") == "e8414a3e-1642-48ec-829e-5f4040e16da9"))
+            else if ((debugEvent is IDebugEntryPointEvent2) || (riidEvent.ToString("D") == "e8414a3e-1642-48ec-829e-5f4040e16da9"))
             {
                 // This is place for place initialisation method 
                 Output.Log("debugEvent is IDebugEntryPointEvent2"); //"IDebugEntryPointEvent2","e8414a3e-1642-48ec-829e-5f4040e16da9"
                 addMainBreakpoint();
+                logbreakpoints();
             }
-            else if (/*(debugEvent is IDebugBreakpointBoundEvent2Guid) || */(riidEvent.ToString("B") == "1dddb704-cf99-4b8a-b746-dabb01dd13a0"))
+            else if (/*(debugEvent is IDebugBreakpointBoundEvent2Guid) || */(riidEvent.ToString("D") == "1dddb704-cf99-4b8a-b746-dabb01dd13a0"))
+            {
                 Output.Log("debugEvent is IDebugBreakpointBoundEvent2Guid.{0}", attributes); //"IDebugBreakpointBoundEvent2Guid ","1dddb704-cf99-4b8a-b746-dabb01dd13a0"
-            else if (/*(debugEvent is IEnumDebugBoundBreakpoints2) || */(riidEvent.ToString("B") == "501c1e21-c557-48b8-ba30-a1eab0bc4a74"))
+                logbreakpoints();
+            }
+            else if (/*(debugEvent is IEnumDebugBoundBreakpoints2) || */(riidEvent.ToString("D") == "501c1e21-c557-48b8-ba30-a1eab0bc4a74"))
+            {
                 Output.Log("debugEvent is IEnumDebugBoundBreakpoints2.{0}", attributes); //"IEnumDebugBoundBreakpoints2","501c1e21-c557-48b8-ba30-a1eab0bc4a74"
-            else if ((debugEvent is IDebugCurrentThreadChangedEvent100) || (riidEvent.ToString("B") == "8764364b-0c52-4c7c-af6a-8b19a8c98226"))
-                Output.Log("debugEvent is IDebugCurrentThreadChangedEvent100 .{0}", attributes); //"IDebugCurrentThreadChangedEvent100  ","8764364b-0c52-4c7c-af6a-8b19a8c98226"
-            
-            else if ((debugEvent is IDebugCurrentThreadChangedEvent100) || (riidEvent.ToString("B") == "8764364b-0c52-4c7c-af6a-8b19a8c98226"))
+                logbreakpoints();
+            }
+            else if ((debugEvent is IDebugCurrentThreadChangedEvent100) || (riidEvent.ToString("D") == "8764364b-0c52-4c7c-af6a-8b19a8c98226"))
                 Output.Log("debugEvent is IDebugCurrentThreadChangedEvent100 .{0}", attributes); //"IDebugCurrentThreadChangedEvent100  ","8764364b-0c52-4c7c-af6a-8b19a8c98226"
 
-            else if ((debugEvent is IDebugProcessContinueEvent100) || (riidEvent.ToString("B") == "c703ebea-42e7-4768-85a9-692eecba567b"))
+            else if ((debugEvent is IDebugCurrentThreadChangedEvent100) || (riidEvent.ToString("D") == "8764364b-0c52-4c7c-af6a-8b19a8c98226"))
+                Output.Log("debugEvent is IDebugCurrentThreadChangedEvent100 .{0}", attributes); //"IDebugCurrentThreadChangedEvent100  ","8764364b-0c52-4c7c-af6a-8b19a8c98226"
+
+            else if ((debugEvent is IDebugProcessContinueEvent100) || (riidEvent.ToString("D") == "c703ebea-42e7-4768-85a9-692eecba567b"))
+            {
                 Output.Log("debugEvent is IDebugProcessContinueEvent100.{0}", attributes); //"IDebugProcessContinueEvent100 ","c703ebea-42e7-4768-85a9-692eecba567b"
-            else if ((debugEvent is IDebugThreadDestroyEvent2) || (riidEvent.ToString("B") == "2c3b7532-a36f-4a6e-9072-49be649b8541"))
+                //RedirectStdStreams(thread);
+            }
+                
+            else if ((debugEvent is IDebugThreadDestroyEvent2) || (riidEvent.ToString("D") == "2c3b7532-a36f-4a6e-9072-49be649b8541"))
                 Output.Log("debugEvent is IDebugThreadDestroyEvent2.{0}", attributes); //"IDebugThreadDestroyEvent2","2c3b7532-a36f-4a6e-9072-49be649b8541"
-            else if ((debugEvent is IDebugProgramDestroyEvent2) || (riidEvent.ToString("B") == "e147e9e3-6440-4073-a7b7-a65592c714b5"))
+            else if ((debugEvent is IDebugProgramDestroyEvent2) || (riidEvent.ToString("D") == "e147e9e3-6440-4073-a7b7-a65592c714b5"))
                 Output.Log("debugEvent is IDebugProgramDestroyEvent2.{0}", attributes); //"IDebugProgramDestroyEvent2","e147e9e3-6440-4073-a7b7-a65592c714b5"
-            else if ((debugEvent is IDebugProcessDestroyEvent2) || (riidEvent.ToString("B") == "3e2a0832-17e1-4886-8c0e-204da242995f"))
+            else if ((debugEvent is IDebugProcessDestroyEvent2) || (riidEvent.ToString("D") == "3e2a0832-17e1-4886-8c0e-204da242995f"))
                 Output.Log("debugEvent is IDebugProcessDestroyEvent2.{0}", attributes); //"IDebugProcessDestroyEvent2","3e2a0832-17e1-4886-8c0e-204da242995f"
-            else if ((debugEvent is IDebugSessionDestroyEvent2) || (riidEvent.ToString("B") == "f199b2c2-88fe-4c5d-a0fd-aa046b0dc0dc"))
+            else if ((debugEvent is IDebugSessionDestroyEvent2) || (riidEvent.ToString("D") == "f199b2c2-88fe-4c5d-a0fd-aa046b0dc0dc"))
             {
                 Output.Log("debugEvent is IDebugSessionDestroyEvent2.{0}", attributes); //"IDebugSessionDestroyEvent2","f199b2c2-88fe-4c5d-a0fd-aa046b0dc0dc"            
                 isAttached = false;
             }
+            else if ((debugEvent is IDebugMessageEvent2) || (riidEvent.ToString("D") == "3bdb28cf-dbd2-4d24-af03-01072b67eb9e"))
+            {
+                Output.Log("debugEvent is IDebugMessageEvent2.{0}", attributes); //"IDebugMessageEvent2","3bdb28cf-dbd2-4d24-af03-01072b67eb9e"
+                RedirectStdStreams(thread);
+                logbreakpoints();
+            }
+            else if ((debugEvent is IDebugProcessDestroyEvent2) || (riidEvent.ToString("D") == "1dddb704-cf99-4b8a-b746-dabb01dd13a0"))
+            {
+                Output.Log("debugEvent is IDebugBreakpointBoundEvent2.{0}", attributes); //"IDebugBreakpointBoundEvent2","1dddb704-cf99-4b8a-b746-dabb01dd13a0"
+                logbreakpoints();
+            }
             else
-                Output.Log("Event Command.name = {0}.{1}", riidEvent.ToString("B"), attributes);
+                Output.Log("Event Command.name = {0}.{1}", riidEvent.ToString("D"), attributes);
             return VSConstants.S_OK;
         }
     }
