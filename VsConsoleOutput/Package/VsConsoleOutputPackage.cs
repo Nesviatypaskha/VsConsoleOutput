d6656645a0823bd061934d269225b9a4f7409ac5
Nesviatypaskha <alex.nesv@gmail.com>
2019-12-25 00:41:21 +0200

    version 0.9.1

--- VsConsoleOutput/Package/VsConsoleOutputPackage.cs
+++ VsConsoleOutput/Package/VsConsoleOutputPackage.cs
@@ -15,7 +15,7 @@ namespace package
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
 
-    [InstalledProductRegistration("VSConsoleOutput", "VSConsoleOutput", "0.9.0", IconResourceID = 400)]
+    [InstalledProductRegistration("VSConsoleOutput", "VSConsoleOutput", "0.9.1", IconResourceID = 400)]
     public sealed class VSConsoleOutputPackage : AsyncPackage
     {
         public const string PackageGuidString = "f6dfad00-7979-4fd7-b28b-71336c51f20f";
